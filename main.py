12961c1bbe91a1b4b1f867f55a0a32ae82c11b12
eivankin <eug-potato@yandex.ru>
2020-10-20 18:46:46 +0300

    TaskViewer WIP

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
-from views import Ui_SolveViewer
+from views import Ui_SolveViewer, Ui_TaskViewer
 import numpy as np
 import sys
 
@@ -22,8 +22,38 @@ class SolveViewer(QMainWindow, Ui_SolveViewer):
             self.plotter.save(file_name)
 
 
+class TaskViewer(QMainWindow, Ui_TaskViewer):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.action.triggered.connect(self.loadDB)
+        self.action_2.triggered.connect(self.loadCSV)
+
+        self.pushButton_2.clicked.connect(self.solveSelected)
+        self.pushButton_3.clicked.connect(self.deleteSelected)
+        self.pushButton_4.clicked.connect(self.appendTask)
+
+    def loadDB(self):  # TODO
+        pass
+
+    def loadCSV(self):  # TODO
+        pass
+
+    def deleteSelected(self):  # TODO
+        pass
+
+    def solveSelected(self): # TODO
+        pass
+
+    def appendTask(self):  # TODO
+        pass
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = SolveViewer()
-    ex.show()
+    sv = SolveViewer()
+    tv = TaskViewer()
+    sv.show()
+    tv.show()
     sys.exit(app.exec())
