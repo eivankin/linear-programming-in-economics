7cb285998ae5da7371552c6666cc1b4f9b659078
eivankin <eug-potato@yandex.ru>
2020-10-26 23:18:31 +0300

    changed models according to the new database

--- models.py
+++ models.py
@@ -8,26 +8,6 @@ from functools import reduce
 DATABASE = 'tasks_and_settings.db'
 CONNECTION = sqlite3.connect(DATABASE)
 CURSOR = CONNECTION.cursor()
-CURSOR.execute('''CREATE TABLE IF NOT EXISTS Settings (
-                id INTEGER PRIMARY KEY,
-                short_desc TEXT NOT NULL,
-                lc_colors TEXT NOT NULL,
-                tf_color TEXT NOT NULL,
-                fill_colors TEXT NOT NULL
-                )''')
-
-CURSOR.execute('''CREATE TABLE IF NOT EXISTS Tasks (
-                id INTEGER PRIMARY KEY,
-                problem_situation TEXT,
-                target_func_lim TEXT NOT NULL,
-                target_func_coefs TEXT NOT NULL,
-                linear_constraints TEXT NOT NULL,
-                settings_id INTEGER,
-                FOREIGN KEY (settings_id)
-                    REFERENCES Settings (id)
-                        ON DELETE CASCADE 
-                        ON UPDATE NO ACTION
-                )''')
 
 
 class AbstractModel:
@@ -109,12 +89,18 @@ class AbstractModel:
                 for x in CURSOR.execute('SELECT * FROM ' + self.TABLE).fetchall()]
 
     def __get_filter_cursor(self, **kwargs):
+        if not kwargs:
+            raise ValueError('''empty constraints (set it in method call). 
+            Use method "all" to get full list of objects.''')
         return CURSOR.execute(f'''SELECT * FROM {self.TABLE}
             WHERE {" AND ".join(["?=?" * len(kwargs)])}''', (
             *reduce(lambda res, x: res + x, kwargs.items()),
         ))
 
-    def __row_to_obj(self, row):
+    def row_to_obj(self, row):
+        """Преобразует строку таблицы в объект модели.
+        :param row: строка таблицы.
+        :return ModelObject: созданный объект."""
         return self.__ModelObject(self, True, **dict(zip(self.ATTRS, row)))
 
     def get(self, **kwargs):
@@ -122,13 +108,13 @@ class AbstractModel:
         Если объектов, удовлетворяющих ограничениям, несколько, то возвращает первый.
         Если объект не найден, возвращает None"""
         result = self.__get_filter_cursor(**kwargs).fetchone()
-        return result if not result else self.__row_to_obj(result)
+        return result if not result else self.row_to_obj(result)
 
     def filter(self, **kwargs):
         """Возвращает список объектов по заданным ограничениям типа 'аттрибут=значение'.
         Список объектов может быть пустым."""
         result = self.__get_filter_cursor(**kwargs).fetchall()
-        return [self.__row_to_obj(row) for row in result]
+        return [self.row_to_obj(row) for row in result]
 
 
 class SettingsModel(AbstractModel):
@@ -137,12 +123,15 @@ class SettingsModel(AbstractModel):
 
 class TaskModel(AbstractModel):
     TABLE = 'Tasks'
-    LIM_INF = 'inf'
-    LIM_ZERO = '0'
+    LIM_INF = -1
+    LIM_ZERO = 1
     VERBOSE_ATTRS = {
         'problem_situation': 'Условие задачи',
-        'target_func_lim': 'Целевая функция стремиться к',
-        'linear_constraints': 'Линейные ограничения',
-        'settings_id': 'Настройки',
-        'target_func_coefs': 'Коэффициенты целевой функции'
+        'target_func_lim': 'Целевая функция стремится к',
+        'target_func_coefs': 'Коэффициенты ЦФ',
+        'inequalities_coefs': 'Коэффициенты линейных ограничений',
+        'inequalities_consts': 'Значения линейных ограничений',
+        'axis_coefs': 'Коэффициенты осевых ограничений',
+        'axis_consts': 'Значения осевых ограничений',
+        'settings_id': 'Настройки стилей',
     }
